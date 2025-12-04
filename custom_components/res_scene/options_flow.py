b60982d3c623eee17f4d761c90812784cc7961fb
nao-pon <hypweb@gmail.com>
2025-12-05 01:32:42 +0900

    fix: safe rename operations in Options Flow

--- custom_components/res_scene/options_flow.py
+++ custom_components/res_scene/options_flow.py
@@ -1,7 +1,12 @@
+import logging
+
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class ResSceneOptionsFlow(config_entries.OptionsFlow):
@@ -36,10 +41,25 @@ class ResSceneOptionsFlow(config_entries.OptionsFlow):
 
             if rename_from and rename_to != "":
                 if rename_from in manager.stored_data:
-                    manager.stored_data[rename_to] = manager.stored_data.pop(
-                        rename_from
-                    )
-                    await manager.store.async_save(manager.stored_data)
+                    if rename_to in manager.stored_data and rename_to != rename_from:
+                        # Could show an error or warning to the user
+                        _LOGGER.warning(
+                            "Scene %s already exists, skipping rename from %s",
+                            rename_to,
+                            rename_from,
+                        )
+                    else:
+                        manager.stored_data[rename_to] = manager.stored_data.pop(
+                            rename_from
+                        )
+                        await manager.store.async_save(manager.stored_data)
+                        # Dispatch signals for removed and added scenes
+                        async_dispatcher_send(
+                            self.hass, f"{DOMAIN}_scene_removed", rename_from
+                        )
+                        async_dispatcher_send(
+                            self.hass, f"{DOMAIN}_scene_added", rename_to
+                        )
 
             return self.async_create_entry(title="", data=user_input)
 
